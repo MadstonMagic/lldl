85ff7561cd44a5eca06463a40aaf70f655330056
MadstonMagic <madstonpack@gmail.com>
2024-10-14 17:49:24 +0200

    Nieuwe Vragen

--- js/cards.js
+++ js/cards.js
@@ -1,48 +1,62 @@
 $(document).ready( function() {
 	
+	$(function () {
+	    setInterval(function () {
+		    $('.heart-1').fadeOut(350).delay(2000).fadeIn(300).fadeOut(350).delay(1254);
+	        $('.heart-2').fadeOut(500).fadeIn(220).fadeOut(220).delay(1920);
+	        $('.heart-3').fadeOut(250).delay(1800).fadeIn(620).fadeOut(1150).delay(800);
+	        $('.heart-4').fadeOut(800).fadeIn(800).fadeOut(3100).delay(950);
+	        $('.heart-5').fadeOut(475).fadeIn(360).fadeOut(350).delay(2350).delay(1000);
+	        $('.heart-6').fadeOut(650).fadeIn(1100).fadeOut(480).delay(950);
+	        $('.heart-7').fadeOut(300).fadeIn(750).fadeOut(200).delay(550).delay(600);
+	        $('.heart-8').fadeOut(450).fadeIn(700).fadeOut(880).delay(350);
+	    }, 1);
+    });
+	
+	
+	
+	
 	
 	
 	// Disclaimer: Help from ChatGPT
-	// Creation of the array
-	var chooser = randomNoRepeats([
-		// array with 150 strings
+	// Creation of Date array
+	var cardsDate = randomDateNoRepeats([
+		// array with a lot of strings
 		'Liever een lange wandeling in de natuur of losgaan op een feestje',
-		'Ben ik knapper dan je ex',
+		'Ben ik knapper dan je ex?',
 		'Een partner die niet kan communiceren of slecht is in bed?',
 		'Huisje Boompje Beestje of vrij in de wereld?',
 		'Intelligentie of uiterlijk?',
 		'Geef je date een compliment over iets dat hij/zij niet verwacht',
-		'Kijk elkaar voor 1 minuut diep in de ogen aan. Hoe voelt dit?',
-		'Is roken een afknapper om een date af te wijzen?',
-		'Wat heb ik, wat je ex(en) niet hadden?',
+		'Houd elkaars handen vast en kijk elkaar voor 1 minuut diep in de ogen aan. Wat voel je?',
+		'Is roken een afknapper om een eerste date af te wijzen?',
+		'Wat heb ik, wat je ex(en) niet had(den)?',
 		'Zou je jouw date nu een kus willen geven?',
 		'Welk lichaamsdeel vind je het mooiste aan jezelf?',
 		'Welk lichaamsdeel, van wat je nu ziet, vind je het mooiste aan je date?',
 		'Liever knuffelend in slaap vallen of voor het slapen gaan nog seks?',
-		'Liever wat spek aan het bot of een magere sportschool-fanaat?',
+		'Liever wat spek aan het bot of een afgetrainde sportschool-fanaat?',
 		'Geef jouw bedkunsten een cijfer.',
 		'Ben jij van het onderzoeken/experimenteren tussen de lakens?',
-		'Welke fetish wil jij graag met je partner uitproberen?',
 		'Heb je de behoefte om je date te bespringen?',
 		'Graag een dagje weg of bankhangen?',
 		'Liever met de auto op vakantie of het vliegtuig?',
 		'Hecht je veel waarde aan monogamie?',
-		'Heb je ooit een spicy foto gestuurd én ontvangen?',
+		'Heb je ooit een pikante foto gestuurd én ontvangen?',
 		'Wat vind je belangrijker? Een goed gebit, een verzorgde huid of een goede lichaamsgeur?',
 		'Is zoenen met een ander tijdens je relatie vreemdgaan?',
-		'Samen sexuele video\'s kijken, kan dat?',
+		'Samen intieme video\'s kijken, kan dat?',
 		'Is zelfgenot tijdens je relatie een vorm van vreemdgaan?',
 		'Liever je partner die oud kaal wordt of op jonge leeftijd grijs?',
-		'Liever iemand die gelovig is of iemand die zich met complottheori&#235;n bezighoudt?',
+		'Liever iemand die gelovig is of iemand die zich met complottheoriën bezighoudt?',
 		'Stel je blijft slapen op de eerste date, moet de man dan op de bank?',
 		'Is seks meer voor de lust of de liefde?',
 		'Hoe ziet jouw ideale bedpartner er uit?',
-		'Wat zou je tussen de lakens nog willen proberen?',
 		'Hoe ziet jouw ideale sekspartij er uit?',
 		'Liever een massage met of zonder massageolie?',
 		'Val ik een binnen het type waar jij op valt?',
 		'Borsten of billen en waarom?',
-		'Liever iemand die er minder knap is maar vol met humor, of heel erg knap maar zonder humor?',
+		'Liever iemand die er minder knap is maar vol met humor, of heel erg knap maar een saaie kwal?',
 		'Zijn opgespoten lippen, borsten of billen een afknapper?',
 		'Op welk eigenschap ben je zelf erg trots?',
 		'Zit je vaak op je telefoon of niet?',
@@ -51,11 +65,11 @@ $(document).ready( function() {
 		'Heb je een herinnering uit je jeugd wat je misschien zou willen veranderen als dat kon?',
 		'Welke eigenschappen zoek je in een partner?',
 		'Hoe ziet je ideale weekend er uit met je partner?',
-		'Zou je nog op reis willen, zo ja waar naar toe?',
+		'Zou je nog op reis willen? Zo ja waar naar toe?',
 		'Humor is belangrijker dan een \'goed\' lichaam.',
 		'Wel of geen make-up?',
 		'Heb je een piercing of zou je er een willen? Zo ja, waar?',
-		'Een partner met een goed inkomen of met veel liefde voor jou?',
+		'Een partner met een goed inkomen of met veel liefde voor zijn/haar partner?',
 		'Ochtend- of avondmens?',
 		'Zou je voor een dag van geslacht willen wisselen? Zo ja, wat zou je dan doen?',
 		'Wat zou jij aan je uiterlijk willen veranderen?',
@@ -66,27 +80,25 @@ $(document).ready( function() {
 		'Puur, melk of witte chocolade?',
 		'Waar kan je s\'nachts voor wakker gemaakt worden?',
 		'Handboeien of geblinddoekt in de slaapkamer?',
-		'Wat is jouw spirit animal?',
-		'Laat zien wat er op de laatste foto staat die je hebt gemaakt.',
-		'Laat zien wat je meest beluisterde liedje is.',
-		'Wat is de vreemdste situatie die je tijdens een date/relatie ooit hebt meegemaakt?',
+		'Wat is jouw spirit animal?', 
+		'Laat zien wat er op de laatste foto staat die je hebt gemaakt.', 
+		'Wat is de gekste gebeurtenis die je hebt meegemaakt in groepsverband?',
 		'Wat is je grootste angst?',
-		'10 minuten gratis winkelen, waar zou ,je dit doen?',
-		'Ben jij wel of niet dominant in bed?',
+		'10 minuten gratis winkelen, waar zou je dit doen?',
 		'Geef elkaar een welgemeend compliment!',
 		'Heb je thuis seksspeeltjes liggen of ooit gehad?',
 		'Liever slim of grappig zijn?',
 		'Als je op vakantie gaat, liever een hotel of (kamperen) in de natuur?',
 		'Heb je liever een tatoeage of een piercing?',
-		'Liever in de toekomst kunnen kijken of het verleden kunnen veranderen?',
+		'Liever in de toekomst kunnen kijken of het veleden kunnen veranderen?',
 		'Sporten of lekker eten?',
 		'Stel je partner komt in een rolstoel terecht, blijf je dan bij hem/haar?',
 		'Als je mijn uitstraling zou moeten omschrijven, hoe zou je dat dan omschrijven?',
 		'Ben jij een knuffelbeer(tje)?',
-		'Streel je date over zijn/haar gezicht, hoe voelt dit?',
+		'Streel je date over zijn/haar rug, hoe voelt dit?',
 		'Hoe blaas jij iemand omver met een zoen?',
 		'Hoe stel jij je partner op zijn/haar gemak?',
-		'Val je liever knuffelend of met een nachtkus in slaap?',
+		'Val je liever knuffelend in slaap of met een nachtkus?',
 		'Hoe belangrijk vind jij seks en intimiteit in een relatie?',
 		'Ben jij een dominant type of laat jij je liever iets leiden?',
 		'Hoe zie jij jezelf over 10 jaar?',
@@ -108,30 +120,29 @@ $(document).ready( function() {
 		'Wanneer vind jij jezelf sexy?',
 		'Wat is jouw body-count?',
 		'Vind je het belangrijk dat je partner wilt reizen?',
-		'Hoe vaak denk je aan seks, en heb je daar vandaag al aangedacht?',
+		'Hoe vaak denk je aan intimiteit? En heb je daar vandaag al aangedacht?', 
 		'Geef ik je een goed gevoel van binnen?',
 		'Hoe zou je reageren als ik je nu zou zoenen?',
 		'Vind jij jezelf goed in bed?',
-		'Is smakken tijdens het eten een afknapper?',
-		'Wat kies je, Doen of de Waarheid? <span style="font-weight:bold;">Doen</span>: Geef je date een zoen. <span style="font-weight:bold;">Waarheid</span>: Ben je ooit betrapt tijdens seks?',
-		'Je date slaapt graag zonder kleren, vind je dit vervelend?',
-		'Sex op de eerste date, ja of nee?',
+		'Vind je smakken tijdens het eten een (grote) afknapper?',
+		'Geef je date een zoen en vertel wat je voelt.',
+		'Als je partner naakt slaapt maar jij niet, is dat een probleem?',
 		'Ben jij wel of niet dominant in bed?',
 		'Wat vind je er van als je partner veel vrienden hebt van het andere geslacht?',
 		'Je partner wilt graag kinderen maar jij niet, wil je het dan met goede communicatie alsnog overwegen?',
 		'Is een \'goed\' lichaam belangrijk voor goed genot in een seksleven?',
 		'Kan jij goed de eerste stap zetten?',
-		'Ben je fan van netflix & chill?',
+		'Wat vind je van netflix & chill?',
 		'Wat vind je van samen douchen?',
-		'Ben jij een snoepkont?',
+		'Ben jij een snoepkont?', 
 		'Stel je partner heeft tepelpiercings, is dit een afknapper?',
 		'Lichamelijk contact is net zo belangrijk als goede communicatie, eens of oneens?',
 		'Samen een hobby delen is belangrijk voor een goede relatie, waar of niet waar?',
-		'Waar kijk je het eerste naar bij een ander?',
+		'Waar keek je het eerste naar bij je date?',
 		'Kaas- of chocoladefondue?',
 		'Je partner snurkt, is dit een afknapper?',
 		'Je date gebruikt drugs, is dit een afknapper?',
-		'Liever een partner die ietwat ADHD is of iemand die misschien iets te rustig is?',
+		'Liever een partner die ietwat ADHD heeft of iemand die misschien iets te rustig is?',
 		'Massage met of zonder happy-ending?',
 		'Liever een zacht buikje dan een wasbordje?',
 		'Heb je een tatoeage of zou je er een willen? Zo ja, waar?',
@@ -155,9 +166,80 @@ $(document).ready( function() {
 		'Als je nog aan het daten bent, mag je dan ook met anderen afspreken?',
 		'Liever een week samen op een eiland of op een berg?',
 		'Liever met je vrienden of je partner over seks praten?',
+		'Stel je partner wil uit gaan met vrienden of vriendinnen, vind je dat oké?',
+		'Wat is je favoriete liedje en waarom?',
+		'Ben je wel eens gekwetst in de liefde? Zo ja, hoe ging je daar mee om?',
+		'Ben je ergens onzeker over? Zo ja, waarover?',
+		'Hoe laat jij zien dat je geïnteresseerd bent?',
+		'Wat vind je er van als je partner het niet erg vindt zijn/haar emoties te laten zien?',
+		'Wat zijn jouw grootste deal-breakers?',
+		'Hoe ga je om met conflicten in je relatie?',
+		'Welk romantisch gebaar betekend veel voor je?',
+		'Wat wil je altijd al met een partner samen doen?',
+		'Hoe vier je een jubileum in je relatie?',
+		'Wat is je favoriete manier van intimiteit?',
+		'Hoe wordt jij het liefste aangeraakt tijdens je date-periode?',
+		'Vind je sfeer maken vóór seks een belangrijk onderdeel?',
+		'Speelt humor een rol in jouw seksleven?',
+		'Hoe vaak wil je seks per week?',
+		'Wat vind je van praten over seksuele verlangens?',
+		'Heb jij een ondeugende kant in bed die je niet verwacht?'
 	]);
+	
+	// Creation of Partner array
+	var cardsPartner = randomPartnerNoRepeats([
+		// array with a lot of strings
+		'Kijk elkaar voor 1 minuut diep in de ogen aan en probeer elkaar te verleiden',
+		'Doen of de Waarheid? <b>Doen: </b>Streel je partner over zijn/haar been tot je bij de intieme zone komt. <b>Waarheid: </b> Hoe vaak heb je aan seks gedacht vandaag?',
+		'Doen of de Waarheid? <b>Doen :</b>Geef je partner een intieme zoen. <b>Waarheid: </b>Ben je ooit betrapt tijdens seks?',
+		'Welke fetish wil jij graag met mij nog uitproberen?',
+		'Wat zou je tussen de lakens nog willen proberen?',
+		'Probeer je partner in de stemming te krijgen, maar stop te vroeg.',
+		'Beschrijf jouw geslachtsdeel',
+		'Ben jij fan van beffen/gebeft worden?',
+		'Ben jij fan van pijpen/gepijpt worden?',
+		'Lig jij liever boven- of onderop?',
+		'Wat is jouw favoriete standje?',
+		'Doen of de Waarheid? <b>Doen: </b>Ga in je ondergoed naast je partner liggen. <b>Waarheid: </b>Hoe vind jij je partner op zijn/haar sexiest?',
+		'Hoe ziet jouw toekomst er uit met mij?',
+		'Welke fantasieën willen jullie nog ontdekken? Zeg het tegelijkertijd op 3.',
+		'How vind jij het fijn om intiem aangeraakt te worden?',
+		'Wat zorgt ervoor dat jij altijd in de stemming komt?',
+		'Welke sexuele fantasie heb jij nog niet gedeeld met mij?',
+		'Wil je graag seksspeeltjes gebruiken tijdens seks met je partner, en niet alleen tijdens masturbatie?',
+		'Hoe kunnen wij het hoogtepunt leuker maken voor ons beiden? (Niet? hoe zou je het dan interessanter maken?)',
+		'Wat is je meest ontdeugende fantasie dat je nog uit wilt proberen?',
+		'Ben jij open-minded genoeg om andere dingen te proberen? (Bijvoorbeeld anaal, een trio of met speeltjes)',
+		'Wat vind je van orgasme-tegenhoudende technieken?',
+		'Doen of de Waarheid? <b>Doen: </b>Trek het ondergoed van elkaar uit en ga naakt verder. <b>Waarheid: </b> Wat is de meest gekke plek waar je seks hebt gehad?',
+		'Wat vind je van intieme spelletjes? Bijvoorbeeld strippoker of naakt twister.',
+		'Wat vind jij van van mijn geslachtsdeel? En andersom.',
+		'Heb jij gevoelige tepels? Test het uit!',
+		'Zoen je partner sensueel in de nek en let op zijn/haar ademhaling en/of hartslag',
+		'Doe je veel aan masturbatie?',
+	]);
+	
+	
+	// error message when getting to the final question
+	var errorMessage = "Je hebt alle kaarten gehad! Zodra we nieuwe vragen hebben toegevoegd, vind je dat aan de linkerkant!<br>Nog een keer spelen? Ververs de pagina!";
+	
+	// Date Cards
+	function randomDateNoRepeats(array) {
+		var copy = array.slice(0); // Make a copy of the original array
 
-	function randomNoRepeats(array) {
+		return function() {
+			if (copy.length === 0) {
+				return null; // Stop once all items are picked
+			}
+			var index = Math.floor(Math.random() * copy.length);
+			var item = copy[index];
+			copy.splice(index, 1); // Remove the picked item
+			return item;
+		};
+	}
+	
+	// Partner Cards
+	function randomPartnerNoRepeats(array) {
 		var copy = array.slice(0); // Make a copy of the original array
 
 		return function() {
@@ -171,17 +253,30 @@ $(document).ready( function() {
 		};
 	}
 
-	// Click handler for the button
+	// Click handler for the button - Date Cards
 	$('.question-btn-wrapper').on('click', function() {
 		// Get the next random element from the chooser
-		let result = chooser();
+		let result = cardsDate();
 
 		if (result !== null) {
 			// Update the HTML with the randomly chosen string
 			$(".cardTextArea .cardText").html(result);
 		} else {
-			// When all elements have been picked, display a message
-			$(".cardTextArea .cardText").html("Je hebt alle kaarten gehad! Zodra we nieuwe vragen hebben toegevoegd, vind je dat aan de linkerkant!<br>Nog een keer spelen? Ververs de pagina!");
+			$(".cardTextArea .cardText").html(errorMessage);
+		}
+	});
+
+
+	// Click handler for the button - Partner Cards
+	$('.partner-question-btn-wrapper').on('click', function() {
+		// Get the next random element from the chooser
+		let result = cardsPartner();
+
+		if (result !== null) {
+			// Update the HTML with the randomly chosen string
+			$(".cardTextArea .cardText").html(result);
+		} else {
+			$(".cardTextArea .cardText").html(errorMessage);
 		}
 	});
 });
